82ca36441efc4607ad91f2fc94b659e60347d9bd
Risqi RA <risqiraacc@gmail.com>
2022-07-13 21:31:11 +0700

    add fungsi enumerate via branch fungsi-enumerate

--- Challenge/Python/Python-Challenge-RRA/Day 2/Fungsi Bawaan PY/-ALLFungsi.py
+++ Challenge/Python/Python-Challenge-RRA/Day 2/Fungsi Bawaan PY/-ALLFungsi.py
@@ -524,3 +524,75 @@ print(dir(number1))
 # Output : ['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
 
 # ────────────────────────────────────────────────────────────────────────────────
+
+# TODO ─── Penggunaan Fungsi Enumerate ────────────────────────────────────────────────
+# Metode enumerate()ini menambahkan penghitung ke iterable dan mengembalikannya(objek enumerate).
+# Syntax : enumerate(iterable, start=0)
+# enumerate()metode membutuhkan dua parameter:
+# iterable - urutan, iterator, atau objek yang mendukung iterasi
+# start(optional) - enumerate() mulai menghitung dari nomor ini. Jika start dihilangkan, is diambil sebagai awal..
+# enumerate()metode menambahkan penghitung ke iterable dan mengembalikannya.
+# Objek yang dikembalikan adalah objek enumerate.
+# #
+
+# ─── Contoh 0 ───────────────────────────────────────────────────────────────────
+
+languages = ['Python', 'Java', 'JavaScript']
+
+enumerate_prime = enumerate(languages)
+
+# convert enumerate object to list
+print(list(enumerate_prime))
+
+# Output: [(0, 'Python'), (1, 'Java'), (2, 'JavaScript')]
+
+# ────────────────────────────────────────────────────────────────────────────────
+
+# ─── Contoh 1 : Bagaimana Enumerate Bekerja ─────────────────────────────────────
+
+grocery = ['bread', 'milk', 'butter']
+enumerateGrocery = enumerate(grocery)
+
+print(type(enumerateGrocery))
+# Output : <class 'enumerate'>
+
+# converting to list
+print(list(enumerateGrocery))
+#Output : [(0, 'bread'), (1, 'milk'), (2, 'butter')]
+
+# ketika menghitung mulai dari angka yang ditentukan
+enumerateGrocery = enumerate(grocery, 10)
+print(list(enumerateGrocery))
+# Output : [(10, 'bread'), (11, 'milk'), (12, 'butter')]
+
+# ────────────────────────────────────────────────────────────────────────────────
+
+# ─── Contoh 2: Perulangan Obyek Enumerate ────────────────────────────────
+grocery = ['bread', 'milk', 'butter']
+
+for item in enumerate(grocery):
+    print(item)
+    # Output :
+    # (0, 'bread')
+    # (1, 'milk')
+    # (2, 'butter')
+print('\n')
+
+
+for count, item in enumerate(grocery):
+    print(count, item)
+    # 0 bread
+    # 1 milk
+    # 2 butter
+print('\n')
+
+
+# changing default start value
+for count, item in enumerate(grocery, 100):
+    print(count, item)
+# 0 bread
+# 1 milk
+# 2 butter
+
+
+# ────────────────────────────────────────────────────────────────────────────────
