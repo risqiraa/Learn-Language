8302815ef09e2c7937eac887335788cc6b9775fd
Risqi RA <risqiraacc@gmail.com>
2022-07-13 20:06:35 +0700

    add fungsi dir() menggunakan branch fungsi-dir

--- Challenge/Python/Python-Challenge-RRA/Day 2/Fungsi Bawaan PY/-ALLFungsi.py
+++ Challenge/Python/Python-Challenge-RRA/Day 2/Fungsi Bawaan PY/-ALLFungsi.py
@@ -436,3 +436,49 @@ print('z = ', point1.z)
 
 # ────────────────────────────────────────────────────────────────────────────────
 # ────────────────────────────────────────────────────────────────────────────────
+
+# ─── Penggunaan Fungsi Dir ──────────────────────────────────────────────────────
+# Metode dir()mengembalikan daftar atribut yang valid dari objek yang diteruskan.
+# Syntax : dir(object)
+# Metode dir()ini mengambil satu parameter:
+# objek - bisa berupa tupel kosong/terisi, daftar, set, kamus dll atau objek yang ditentukan pengguna
+# Metode dir()mengembalikan daftar atribut objek yang diteruskan ke metode
+# #
+
+# ─── Contoh 0 ───────────────────────────────────────────────────────────────────
+number = [12]
+# returns valid attributes of the number list
+print(dir(number))
+# Output: ['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
+
+# ─── Contoh 1 : Python dir() dengan List───────────────────────────────────────────────────────────────────
+
+number1 = [1, 2, 3]
+
+# dir with a filled list
+print(dir(number1))
+# Output: ['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
+number2 = []
+
+# dir() with an empty list
+print(dir(number2))
+# Output: ['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
+# ────────────────────────────────────────────────────────────────────────────────
+
+
+# ─── Contoh 2 : Python dir() Dengan Set ───────────────────────────────────────────
+number = {12, 15, 18, 21}
+
+# dir() with a filled set
+print(dir(number))
+# Output : ['__and__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__iand__', '__init__', '__init_subclass__', '__ior__', '__isub__', '__iter__', '__ixor__', '__le__', '__len__', '__lt__', '__ne__', '__new__', '__or__', '__rand__', '__reduce__', '__reduce_ex__', '__repr__', '__ror__', '__rsub__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__xor__', 'add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 'intersection', 'intersection_update', 'isdisjoint', 'issubset', 'issuperset', 'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 'union', 'update']
+number1 = []
+
+# dir() with an empty set
+print(dir(number1))
+# Output : ['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
+# ────────────────────────────────────────────────────────────────────────────────
