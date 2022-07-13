c9d7521f5d3772d9caa39912161e4049e2a88814
Risqi RA <risqiraacc@gmail.com>
2022-07-13 12:23:54 +0700

    Day 2 + 13 File - comile,classmethod

--- Challenge/Python/Python-Challenge-RRA/Day 2/Fungsi Bawaan PY/-ALLFungsi.py
+++ Challenge/Python/Python-Challenge-RRA/Day 2/Fungsi Bawaan PY/-ALLFungsi.py
@@ -2,6 +2,7 @@
 # abs() digunakan untuk mengembalikan nilai mutlak suatu bilangan
 # misalnya |3| = 3 dan |-3|=3
 
+from datetime import date
 print("abs(-15) : ", abs(-15))  # 15
 print("abs(10.32) : ", abs(10.32))  # 10.32
 print("abs(-100) : ", abs(-100))  # 100
