9569b316e82df04a76913685a533b0ec4b2a0d59
Risqi RA <risqiraacc@gmail.com>
2022-07-14 11:38:10 +0700

    add eval break v1.0

---
+++ Challenge/Python/Python-Challenge-RRA/Day 2/Fungsi Bawaan PY/eval.py
@@ -0,0 +1,11 @@
+# ─── Penggunaan Fungsi Eval ─────────────────────────────────────────────────────
+# Metode eval()ini mem-parsing ekspresi yang diteruskan ke metode ini dan menjalankan ekspresi python (kode) di dalam program.
+# Syntax : eval(expression, globals=None, locals=None)
+# The eval() function takes three parameters:
+# ekspresi - string diuraikan dan dievaluasi sebagai ekspresi Python
+# global (opsional) - kamus
+# penduduk setempat(opsional) - objek pemetaan. Kamus adalah tipe pemetaan standar dan umum digunakan dalam Python.
+# penggunaan dari global dan locals akan dibahas nanti di artikel ini.
+# 
+# Metode eval()mengembalikan hasil yang dievaluasi dariekspresi.
+# #
